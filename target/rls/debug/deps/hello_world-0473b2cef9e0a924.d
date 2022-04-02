7f04a3134d9c904e5450e318a6c134fd7397c247
Mac DeCourcy <49794076+mdecourcy@users.noreply.github.com>
2022-04-01 22:48:30 -0700

    Delete target directory

--- target/rls/debug/deps/hello_world-0473b2cef9e0a924.d
+++
@@ -1,9 +0,0 @@
-/home/macdecourcy/Code/rust/learning/hello-rust/target/rls/debug/deps/hello_world-0473b2cef9e0a924.rmeta: src/main.rs src/print.rs src/vars.rs src/types.rs src/strings.rs
-
-/home/macdecourcy/Code/rust/learning/hello-rust/target/rls/debug/deps/hello_world-0473b2cef9e0a924.d: src/main.rs src/print.rs src/vars.rs src/types.rs src/strings.rs
-
-src/main.rs:
-src/print.rs:
-src/vars.rs:
-src/types.rs:
-src/strings.rs:
