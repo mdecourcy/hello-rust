21b72470812aea6afe4348e92d593ca9fcb84de5
Mac DeCourcy <macdecourcy@pop-os.localdomain>
2022-04-02 08:44:39 -0700

    vectors.rs complete

--- src/vectors.rs
+++ src/vectors.rs
@@ -7,6 +7,13 @@ pub fn run(){
     // re assign
     numbers[2] = 20;
 
+    //add to vec
+    numbers.push(5);
+    numbers.push(7);
+
+    // pop
+    numbers.pop();
+
     println!("{:?}", numbers);
 
     // Get single val
@@ -22,4 +29,18 @@ pub fn run(){
     let slice: &[i32] = &numbers[0..2];
     println!("Slice: {:?}", slice);
     println!("vector occupies {} bytes", mem::size_of_val(&slice));
+
+    // loop through vector val
+
+    for x in numbers.iter() {
+        println!("Number: {}", x);
+    }
+
+    //loop and mutate val
+    for x in numbers.iter_mut() {
+        *x *= 2;
+    }
+
+    println!("Numbers vec: {:?}", numbers);
+    
 }
